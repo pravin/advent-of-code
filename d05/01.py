14327e217d84bed9a5eb892ef852a36d9fa153b2
Pravin Paratey <pravin@paratey.com>
2017-12-05 16:20:24 +0000

    [Day 05] Refactored code

--- d05/01.py
+++ d05/01.py
@@ -1,36 +1,29 @@
 #!/usr/bin/env python3
 
+def calc(input, part2):
+    steps = 0
+    pos = 0
+    while True:
+        newpos = pos + input[pos]
+        if part2 and (newpos - pos >= 3):
+            input[pos] -= 1
+        else:
+            input[pos] += 1
+        pos = newpos
+        steps += 1
+        if pos >= len(input) or pos < 0: break
+    return steps
+
+
+# Example
+print(calc([0, 3, 0, 1, -3], True))
+print(calc([0, 3, 0, 1, -3], False))
+
+input = []
+
 # Part 1
-input = [0, 3, 0, 1, -3]
-with open('input.txt', 'r') as fp:
-    input = [int(x) for x in fp.read().split()]
-
-steps = 0
-pos = 0
-while True:
-    newpos = pos + input[pos]
-    input[pos] += 1
-    pos = newpos
-    steps += 1
-    if pos >= len(input) or pos < 0: break
-
-print(steps)
-
+with open('input.txt', 'r') as fp: input = [int(x) for x in fp.read().split()]
+print(calc(input, False))
 # Part 2
-input = [0, 3, 0, 1, -3]
-with open('input.txt', 'r') as fp:
-    input = [int(x) for x in fp.read().split()]
-
-steps = 0
-pos = 0
-while True:
-    newpos = pos + input[pos]
-    if newpos - pos >= 3:
-         input[pos] -= 1
-    else:
-        input[pos] += 1
-    steps += 1
-    pos = newpos
-    if pos >= len(input) or pos < 0: break
-
-print(steps)
+with open('input.txt', 'r') as fp: input = [int(x) for x in fp.read().split()]
+print(calc(input, True))
