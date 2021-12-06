a68fd0db964d384805774ae723195be3c3976886
Pravin Paratey <pravin@paratey.com>
2021-12-06 16:58:40 +0000

    Day 06 - Part 2

--- 2021/d06/d06.py
+++ 2021/d06/d06.py
@@ -2,17 +2,22 @@
 
 inp = [1,3,4,1,5,2,1,1,1,1,5,1,5,1,1,1,1,3,1,1,1,1,1,1,1,2,1,5,1,1,1,1,1,4,4,1,1,4,1,1,2,3,1,5,1,4,1,2,4,1,1,1,1,1,1,1,1,2,5,3,3,5,1,1,1,1,4,1,1,3,1,1,1,2,3,4,1,1,5,1,1,1,1,1,2,1,3,1,3,1,2,5,1,1,1,1,5,1,5,5,1,1,1,1,3,4,4,4,1,5,1,1,4,4,1,1,1,1,3,1,1,1,1,1,1,3,2,1,4,1,1,4,1,5,5,1,2,2,1,5,4,2,1,1,5,1,5,1,3,1,1,1,1,1,4,1,2,1,1,5,1,1,4,1,4,5,3,5,5,1,2,1,1,1,1,1,3,5,1,2,1,2,1,3,1,1,1,1,1,4,5,4,1,3,3,1,1,1,1,1,1,1,1,1,5,1,1,1,5,1,1,4,1,5,2,4,1,1,1,2,1,1,4,4,1,2,1,1,1,1,5,3,1,1,1,1,4,1,4,1,1,1,1,1,1,3,1,1,2,1,1,1,1,1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,2,5,1,2,1,1,1,1,1,1,1,1,1]
 
-def part1(arr, days):
-    for i in range(days):
-        addcount = 0 # array to add additional fish to the end of the array
-        for x in range(len(arr)):
-            if arr[x] == 0:
-                arr[x] = 6
-                addcount += 1
-            else:
-                arr[x] -= 1
-        arr.extend([8]*addcount)
-    return arr
+def solve(vec, days):
+    for j in range(days):
+        zero = vec[0]
+        for i in range(1, 9):
+            vec[i-1] = vec[i] # reduce internal timer of fishes
+        vec[8] = zero # new lantern fishes born
+        vec[6] += zero # lantern fishes with zero timer reset
 
-print(len(part1(inp, 80)))
+    return sum(vec)
+
+def arr2vec(arr):
+    vec = [0] * 9
+    for x in arr:
+        vec[x] += 1
+    return vec
+
+print(solve(arr2vec(inp), 80))
+print(solve(arr2vec(inp), 256))
 
