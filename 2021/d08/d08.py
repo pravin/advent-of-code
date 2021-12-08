2040f75f6a8327ecfc8ce0872e0af682fd22f51b
Pravin Paratey <pravin@paratey.com>
2021-12-08 16:08:28 +0000

    Day 08: Part 1

---
+++ 2021/d08/d08.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+inp = []
+with open('08.txt') as fp:
+	for line in fp:
+		signal_patterns, output_value = line.strip().split('|')
+		p1 = signal_patterns.strip().split(' ')
+		p2 = output_value.strip().split(' ')
+		inp.append((p1, p2))
+
+def part1(arr):
+	count = 0
+	for p1, p2 in arr:
+		for x in p2:
+			if len(x) in [2, 3, 4, 7]:
+				count += 1
+	return count
+
+print(part1(inp))
