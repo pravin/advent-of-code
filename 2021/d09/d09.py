ea8ea756a5c5dfcc851722e3b6fad9b27325c53e
pravin <pravin@kaizan.ai>
2021-12-10 23:42:24 +0000

    Day 09: Part 2

--- 2021/d09/d09.py
+++ 2021/d09/d09.py
@@ -11,31 +11,54 @@ with open(filename[DEBUG]) as fp:
         inp.append([int(x) for x in line.strip()])
 
 
-def part1(arr):
+def part1():
     count = 0
-    for y in range(len(arr)):
-        for x in range(len(arr[y])):
+    for y in range(len(inp)):
+        for x in range(len(inp[y])):
             neighbours = []
             # Go Up
             if y > 0:
-                neighbours.append(arr[y - 1][x])
+                neighbours.append(inp[y - 1][x])
             # Go Down
-            if y < len(arr) - 1:
-                neighbours.append(arr[y + 1][x])
+            if y < len(inp) - 1:
+                neighbours.append(inp[y + 1][x])
             # Go Left
             if x > 0:
-                neighbours.append(arr[y][x - 1])
+                neighbours.append(inp[y][x - 1])
             # Go Right
-            if x < len(arr[y]) - 1:
-                neighbours.append(arr[y][x + 1])
+            if x < len(inp[y]) - 1:
+                neighbours.append(inp[y][x + 1])
             # Is the current element the smallest amongst its neighbours?
-            if arr[y][x] < min(neighbours):
-                count += arr[y][x] + 1
+            if inp[y][x] < min(neighbours):
+                count += inp[y][x] + 1
     return count
 
 
-def part2(arr):
-    pass
+def part2():
+    sizes = []
+    for y in range(len(inp)):
+        for x in range(len(inp[y])):
+            if inp[y][x] not in (-1, 9):
+                sizes.append(dfs(x, y))
+    sizes.sort()
+    sizes.reverse()
+    return sizes[0] * sizes[1] * sizes[2]
 
 
-print(part1(inp))
+def dfs(x, y):
+    count = 1
+    inp[y][x] = -1  # -1 => visited
+    if y > 0 and inp[y - 1][x] not in (-1, 9):  # Go up
+        count += dfs(x, y - 1)
+    if y < len(inp) - 1 and inp[y + 1][x] not in (-1, 9):  # Go down
+        count += dfs(x, y + 1)
+    if x > 0 and inp[y][x - 1] not in (-1, 9):  # Go left
+        count += dfs(x - 1, y)
+    if x < len(inp[y]) - 1 and inp[y][x + 1] not in (-1, 9):  # Go right
+        count += dfs(x + 1, y)
+
+    return count
+
+
+print(part1())
+print(part2())
