e2551cc0348f9eb9ddf7e06c7862a28626fb0ea6
Pravin Paratey <pravin@paratey.com>
2022-12-02 21:22:43 +0000

    Day 2 (Part 2)

--- 2022/d02/d02.py
+++ 2022/d02/d02.py
@@ -20,5 +20,33 @@ def part1():
     return scores_list
 
 
+def part2():
+    game_map = {
+        "A": {  # Rock chosen
+            "X": 3,  # X: You lose. 3: You chose scissors
+            "Y": 1,  # Y: Draw. 1: You chose rock
+            "Z": 2,  # Z: You win. 2: You chose paper
+        },
+        "B": {  # Paper chosen
+            "X": 1,
+            "Y": 2,
+            "Z": 3,
+        },
+        "C": {
+            "X": 2,
+            "Y": 3,
+            "Z": 1,
+        },
+    }
+
+    score = 0
+    with open("02.txt") as fp:
+        for line in fp:
+            they, outcome = line.strip().split()
+            score = score + game_map[they][outcome] + (ord(outcome) - ord("X")) * 3
+    return score
+
+
 if __name__ == "__main__":
     print(sum(part1()))
+    print(part2())
